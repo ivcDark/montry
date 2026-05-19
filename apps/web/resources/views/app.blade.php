b81519ab9ea90e95b16a216375c90bdcb93662d3
ivanovjju <ivanovvjju@gmail.com>
2026-05-19 21:36:32 +0400

    Установка фавиконки

--- apps/web/resources/views/app.blade.php
+++ apps/web/resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title data-inertia>{{ config('app.name', 'Montri') }}</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
     @vite(['resources/css/app.css', 'resources/js/app.ts'])
     @inertiaHead
